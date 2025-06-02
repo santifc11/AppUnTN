18afb9282d801eb1ac4a05204cafdacdaa142c1d
FelipeCliment <felipe-climent@estudiante.mdp.utn.edu.ar>
2025-06-02 10:55:21 -0300

    Update Administrator_0.java
    creacion del admin0 cuando se inicia el programa

--- src/main/java/utn/TpFinal/AppUnTN/model/Administrator_0.java
+++ src/main/java/utn/TpFinal/AppUnTN/model/Administrator_0.java
@@ -1,4 +1,34 @@
 package utn.TpFinal.AppUnTN.model;
 
-public class Administrator_0 {
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import utn.TpFinal.AppUnTN.repository.UserRepository;
+
+@Component
+@RequiredArgsConstructor
+public class Administrator_0 implements CommandLineRunner {
+
+    private UserRepository userRepo;
+
+    @Override
+    public void run(String... args){
+        String email="admin0@gmail.com";
+        if(userRepo.findByMail(email).isEmpty()){
+            User Admin0=new User();
+            Admin0.setName("Felipe");
+            Admin0.setLastname("Climent");
+            Admin0.setUsername("Admin0");
+            Admin0.setMail(email);
+            Admin0.setCity("San Manuel");
+            Admin0.setPassword("admin0");
+            Admin0.setRole(Role.ADMIN);
+            Admin0.setAbout("Admin 0");
+            userRepo.save(Admin0);
+            System.out.println("Admin 0 creado exitosamente!");
+        }else {
+            System.out.println("Admin 0 ya existía!");
+        }
+
+    }
 }
