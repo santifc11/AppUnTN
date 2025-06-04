7d88805e34764319286558e762dce2c4dcceb366
FelipeCliment <felipe-climent@estudiante.mdp.utn.edu.ar>
2025-06-04 18:16:01 -0300

    termine bien el admin0 , configuracion de la password para que se ecryptada

--- src/main/java/utn/TpFinal/AppUnTN/model/Administrator_0.java
+++ src/main/java/utn/TpFinal/AppUnTN/model/Administrator_0.java
@@ -1,7 +1,9 @@
 package utn.TpFinal.AppUnTN.model;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import utn.TpFinal.AppUnTN.repository.UserRepository;
 
@@ -9,19 +11,22 @@ import utn.TpFinal.AppUnTN.repository.UserRepository;
 @RequiredArgsConstructor
 public class Administrator_0 implements CommandLineRunner {
 
+    @Autowired
     private UserRepository userRepo;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public void run(String... args){
         String email="admin0@gmail.com";
         if(userRepo.findByMail(email).isEmpty()){
-            User Admin0=new User();
+            User Admin0= new User();
             Admin0.setName("Felipe");
             Admin0.setLastname("Climent");
             Admin0.setUsername("Admin0");
             Admin0.setMail(email);
             Admin0.setCity("San Manuel");
-            Admin0.setPassword("admin0");
+            Admin0.setPassword(passwordEncoder.encode("admin0"));
             Admin0.setRole(Role.ADMIN);
             Admin0.setAbout("Admin 0");
             userRepo.save(Admin0);
