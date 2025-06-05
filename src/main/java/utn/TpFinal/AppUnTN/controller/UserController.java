af5c919ab3eafba7093eac8fd64f6d59518c4936
Santiago Fernández Cuello <santiagofc2002@gmail.com>
2025-06-05 15:21:52 -0300

    Index y Register HTML
    index.html y register.html funcionando y anda el registro. todo 10 puntos

--- src/main/java/utn/TpFinal/AppUnTN/controller/UserController.java
+++ src/main/java/utn/TpFinal/AppUnTN/controller/UserController.java
@@ -3,13 +3,14 @@ package utn.TpFinal.AppUnTN.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import utn.TpFinal.AppUnTN.model.User;
 import utn.TpFinal.AppUnTN.service.UserService;
 
 import java.util.List;
 
-@RestController
+@Controller //estaba@RestController pero lo tuve que cambiar para que entre html.
 @RequestMapping("/api/users")
 public class UserController {
 
@@ -17,7 +18,7 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<User> register(@RequestBody User user){
+    public ResponseEntity<User> register(@ModelAttribute User user){ //Model Attribute para que acepte datos de formulario.
         return ResponseEntity.ok(userService.register(user));
     }
 
--- src/main/java/utn/TpFinal/AppUnTN/menu/UI.java
+++
@@ -1,7 +0,0 @@
-package utn.TpFinal.AppUnTN.menu;
-
-public class UI {
-    public static void main(String[] args) {
-        
-    }
-}
