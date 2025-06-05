27ed0054e36eb27e1b99b42e1dc08589e51fecd7
Santiago Fernández Cuello <santiagofc2002@gmail.com>
2025-06-05 12:27:24 -0300

    Queda Andando
    main que vamos a usar de base que ya anda.

--- src/main/java/utn/TpFinal/AppUnTN/model/User.java
+++ src/main/java/utn/TpFinal/AppUnTN/model/User.java
@@ -19,7 +19,6 @@ public class User {
     private String city;
     @Lob //hace que el string pueda ser de mas de 255 caracteres.
     private String about;
-    @Enumerated(EnumType.STRING)
     private Role role; //asigna el rol del usuario
 
 
