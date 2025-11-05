7e09bd0dc49adc774e5e66e79fec4fb8658c04d6
Lautaro <lautaroechebarne@gmail.com>
2025-11-04 23:58:15 -0300

    Integración del back con el front

--- src/main/java/utn/TpFinal/AppUnTN/Security/SecurityConfig.java
+++ src/main/java/utn/TpFinal/AppUnTN/Security/SecurityConfig.java
@@ -54,6 +54,7 @@ public class SecurityConfig {
                                 .requestMatchers(
                                         "/",
                                         "/index.html",
+                                        "/*.js", "/*.css", "/*.ico", "/*.html","/assets/**",
                                         "/login.html",
                                         "/register.html",
                                         "/css/**",
